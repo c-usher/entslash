6af7c743e963bd880b8e3e184e73cf1aa2eef16b
Cody <usher.codyr@gmail.com>
2021-03-24 23:23:34 -0600

    npm install was run

--- src/index.js
+++ src/index.js
@@ -1,39 +1,67 @@
 import Phaser from 'phaser';
-import logoImg from './assets/logo.png';
 
-class MyGame extends Phaser.Scene
-{
-    constructor ()
-    {
-        super();
-    }
 
-    preload ()
-    {
-        this.load.image('logo', logoImg);
-    }
-      
-    create ()
-    {
-        const logo = this.add.image(400, 150, 'logo');
-      
-        this.tweens.add({
-            targets: logo,
-            y: 450,
-            duration: 2000,
-            ease: "Power2",
-            yoyo: true,
-            loop: -1
-        });
-    }
-}
 
 const config = {
+    width: 1280,
+    height: 720,
     type: Phaser.AUTO,
-    parent: 'phaser-example',
-    width: 800,
-    height: 600,
-    scene: MyGame
-};
+    physics: {
+      default: "arcade",
+      arcade: {
+        gravity: { y: 0 },
+        debug: false,
+      },
+    },
+    scale: {
+      mode: Phaser.Scale.FIT,
+      autoCenter: Phaser.Scale.CENTER_BOTH,
+      parent: "game"
+  
+    },
+    pixelArt: true,
+    scene: [WorldScene],
+  };
+  
+  new Phaser.Game(config);
 
-const game = new Phaser.Game(config);
+
+
+// import logoImg from './assets/logo.png';
+
+// class MyGame extends Phaser.Scene
+// {
+//     constructor ()
+//     {
+//         super();
+//     }
+
+//     preload ()
+//     {
+//         this.load.image('logo', logoImg);
+//     }
+      
+//     create ()
+//     {
+//         const logo = this.add.image(400, 150, 'logo');
+      
+//         this.tweens.add({
+//             targets: logo,
+//             y: 450,
+//             duration: 2000,
+//             ease: "Power2",
+//             yoyo: true,
+//             loop: -1
+//         });
+//     }
+// }
+
+// const config = {
+//     type: Phaser.AUTO,
+//     parent: 'phaser-example',
+//     width: 800,
+//     height: 600,
+//     scene: MyGame
+// };
+
+// const game = new Phaser.Game(config);
