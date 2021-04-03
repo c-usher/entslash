ddc1349127efbaa0b4a71741b95f36ec9efb3bab
Cody <usher.cody@outlook.com>
2021-04-03 14:33:30 -0600

    Both title and game over scene have been styled. Game over scene now hase a restart and exit button. Restart button works.

--- src/javascript/classes/scenes/GameOverScene.js
+++ src/javascript/classes/scenes/GameOverScene.js
@@ -4,11 +4,33 @@ export default class GameOverScene extends Phaser.Scene {
   } // Constructor
   preload() {
     this.cameras.main.setBackgroundColor(0xff00ff);
+    this.load.image("gameOverLogo", "/src/assets/you_died_logo.png");
+    this.load.image("gameOverBg", "/src/assets/game_over_scene.png");
+    this.load.image("restartButton", "/src/assets/restart.png");
   } //Preload
   create() {
-    const gameOverText = this.add.text(100, 100, "Game Over!");
-    gameOverText.setInteractive({ useHandCursor: true });
-    gameOverText.on("pointerdown", () => this.clickButton());
+    const gameOverBg = this.add
+      .image(0, 0, "gameOverBg")
+      .setOrigin(0)
+      .setDisplaySize(1280, 720);
+
+    const gameOverLogo = this.add
+      .image(100, -100, "gameOverLogo")
+      .setOrigin(0)
+      .setDisplaySize(1200, 1000);
+
+    const restartButton = this.add
+      .image(200, 150, "restartButton")
+      .setOrigin(0)
+      .setDisplaySize(800, 600);
+
+    const exitButton = this.add
+      .image(175, 240, "exitButton")
+      .setOrigin(0)
+      .setDisplaySize(800, 600);
+
+    restartButton.setInteractive({ useHandCursor: true });
+    restartButton.on("pointerdown", () => this.clickButton());
   }
   clickButton() {
     // const getWorldScene = this.scene.get("WorldScene");
