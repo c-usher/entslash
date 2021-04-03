e7da2d912846b1554a37e4205bb6ae4fac83c9da
Cody <usher.cody@outlook.com>
2021-04-03 13:52:37 -0600

    The tile scene now has a play and exit button. The play button works when clicked.

--- src/javascript/classes/scenes/TitleScene.js
+++ src/javascript/classes/scenes/TitleScene.js
@@ -4,7 +4,10 @@ export default class TitleScene extends Phaser.Scene {
   } // Constructor
   preload() {
     this.cameras.main.setBackgroundColor(0xff00ff);
+    this.load.image("gameLogo", "/src/assets/game_logo.png");
     this.load.image("titleBg", "/src/assets/titleScene.png");
+    this.load.image("playButton", "/src/assets/play_button.png");
+    this.load.image("exitButton", "/src/assets/exit_button.png");
   } //preload
   create() {
     const titleBG = this.add
@@ -12,9 +15,23 @@ export default class TitleScene extends Phaser.Scene {
       .setOrigin(0)
       .setDisplaySize(1280, 720);
 
-    const gameText = this.add.text(100, 100, "Welcome to my game!");
-    gameText.setInteractive({ useHandCursor: true });
-    gameText.on("pointerdown", () => this.clickButton());
+    const gameLogo = this.add
+      .image(100, -100, "gameLogo")
+      .setOrigin(0)
+      .setDisplaySize(1200, 1000);
+
+    const playButton = this.add
+      .image(100, 150, "playButton")
+      .setOrigin(0)
+      .setDisplaySize(800, 600);
+
+    const exitButton = this.add
+      .image(97, 250, "exitButton")
+      .setOrigin(0)
+      .setDisplaySize(800, 600);
+
+    playButton.setInteractive({ useHandCursor: true });
+    playButton.on("pointerdown", () => this.clickButton());
   } //create
   clickButton() {
     this.cameras.main.fade(300, 100, 0, 0);
