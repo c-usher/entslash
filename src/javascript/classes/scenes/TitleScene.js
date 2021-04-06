4b5d3a96180f808aad8526ecfe43140af11abc0f
Cody <usher.cody@outlook.com>
2021-04-06 17:14:24 -0600

    the sprite on the tilescreen now moves when next to the option

--- src/javascript/classes/scenes/TitleScene.js
+++ src/javascript/classes/scenes/TitleScene.js
@@ -22,17 +22,31 @@ export default class TitleScene extends Phaser.Scene {
     playButton.setInteractive({ useHandCursor: true });
     playButton.on("pointerdown", () => this.clickButton());
 
+    let selectorSprite = this.add.sprite(100, 100, "heroSheet");
+    selectorSprite.setVisible(false);
+    this.anims.create({
+      key: "selectorWalk",
+      frameRate: 8,
+      repeat: -1,
+      frames: this.anims.generateFrameNumbers("heroSheet", {
+        start: 3,
+        end: 5,
+      }),
+    });
+
     //When the Player hovers over the playButton choicesSprite will be set to visible. It will show up to the left of the button.
     playButton.on("pointerover", () => {
-      this.anims.play("hero_down", true);
+      selectorSprite.setVisible(true);
+      selectorSprite.play("selectorWalk");
+      selectorSprite.x = playButton.x - 20;
+      selectorSprite.y = playButton.y + 20;
     });
 
     playButton.on("pointerout", () => {
-      choiceSprite.setVisible(false);
+      selectorSprite.setVisible(false);
     });
   } //create
   clickButton() {
-    this.anims.play();
     this.cameras.main.fade(300, 100, 0, 0);
     this.cameras.main.once("camerafadeoutcomplete", () => {
       this.scene.start("WorldScene");
